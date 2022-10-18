be7d3c1f499c27250e9e991d14075fd773a85c09
aldisuryadarma <aldisurya355@gmail.com>
2022-10-18 02:04:03 -0700

    up

---
+++ tools/addons/logo.py
@@ -0,0 +1,15 @@
+# Import modules
+from colorama import Fore
+
+# Logo
+print(rf"""{Fore.MAGENTA}
+ (    (               (        )  
+ )\ ) )\ )            )\ )  ( /(  
+(()/((()/(  (    (   (()/(  )\()) 
+ /(_))/(_)) )\   )\   /(_))((_)\  
+(_)) (_))  ((_) ((_) (_)) __ ((_) 
+/ __|| |   | __|| __|| _ \\ \ / / 
+\__ \| |__ | _| | _| |  _/ \ V /  
+|___/|____||___||___||_|    |_|   
+            created by SleepyBoyZzz   
+{Fore.RESET}""")
